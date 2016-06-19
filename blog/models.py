7a408dcead35a3583b2093da953e3c9824b81089
callisto1337 <hazardous333@gmail.com>
2016-06-20 02:05:13 +0300

    created class for posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length = 50)
+	text = models.TextField()
+	date_created = models.DateTimeField()
+
+	def __str__(self):
+		return self.title

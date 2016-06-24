07fa1a9294e108027d3d8bef70ef13e056bd486c
callisto1337 <hazardous333@gmail.com>
2016-06-24 20:10:06 +0300

    completed development of authorization and registration users

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,29 @@
 from django import forms
-# from django.contrib.auth import login
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+# class FormAuth(forms.Form):
+#     username = forms.CharField(label='Username', max_length=30, required=True)
+#     password = forms.CharField(label='Password', max_length=30, widget=forms.PasswordInput)
 
-class FormAuth(forms.Form):
-    username = forms.CharField(label='Username', max_length=30)
-    password = forms.CharField(label='Password', max_length=30, widget=forms.PasswordInput)
+# class FormReg(FormView):
+#     form_class = UserCreationForm
 
-class FormReg(forms.Form):
-    username = forms.CharField(label='Username', max_length=30)
-    password = forms.CharField(label='Password', max_length=30, widget=forms.PasswordInput)
-    conf_password = forms.CharField(label='Confirm password', max_length=30, widget=forms.PasswordInput)
+#     success_url = "/auth/"
+#     template_name = "blog/reg.html"
+
+#     def form_valid(self, form):
+#         form.save()
+#         return super(RegisterFormView, self).form_valid(form)
+
+# class FormReg(forms.Form):
+#     username = forms.CharField(label='Username', max_length=30, min_length=6, required=True)
+#     email = forms.EmailField(label='Email', max_length=50, min_length=6)
+#     password = forms.CharField(label='Password', max_length=30, widget=forms.PasswordInput, min_length=6)
+#     conf_password = forms.CharField(label='Confirm password', max_length=30, widget=forms.PasswordInput, min_length=6)
+
+#     def validate(self, value):
+#     	data = self.cleaned_data['recipients']
+#     	if password != conf_password:
+#     		raise forms.ValidationError("You have forgotten about Fred!")
+#     	return data
--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,36 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
-
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Post
-from .forms import FormAuth, FormReg
-
 
 def post_list(request):
 	posts = Post.objects.all()[:5]
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def auth(request):
-	if request.user.is_authenticated():
-		return redirect('/')
-	if request.method == 'POST':
-		form_auth = FormAuth(request.POST)
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			login(request, user)
-			return redirect('/')
-		else:
-			return render(request, 'blog/auth.html', {'form_auth': form_auth})
-	else:
-		form_auth = FormAuth()
-	return render(request, 'blog/auth.html', {'form_auth': form_auth})
+    if request.user.is_authenticated():
+        return redirect('/')
+    elif request.method == 'POST':
+        form_auth = AuthenticationForm(request=request, data=request.POST)
+        if form_auth.is_valid():
+            user = form_auth.get_user()
+            login(request, user)
+            return redirect("/")
+    else:
+        form_auth = AuthenticationForm()
+    return render(request, "blog/auth.html", {'form_auth': form_auth})
 
 def reg(request):
-	form_reg = FormReg()
 	if request.user.is_authenticated():
 		return redirect('/')
-	return render(request, 'blog/reg.html', {'form_reg': form_reg})
+	elif request.method == 'POST':
+		form_reg = UserCreationForm(request.POST)
+		if form_reg.is_valid():
+			new_user = form_reg.save()
+			return redirect('/auth/')
+	else:
+		form_reg = UserCreationForm()
+	return render(request, "blog/reg.html", {'form_reg': form_reg})
 
 def logout_view(request):
 	logout(request)
